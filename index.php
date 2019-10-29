3bf0559a6b44fbc243ef75e56bf22b6bdd16f54b
valdas-v1 <valdas00722@gmail.com>
2019-10-29 17:49:19 +0200

    uzduotis #3

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
+<?php
+date_default_timezone_set('Europe/Vilnius');
+?>
 <html lang='en'>
     <head>
-        <title>Komentarai</title>
+        <title>PHP lydės ir <?php print date('Y/m/d', strtotime('-' . rand() . 'hours')); ?></title>
     </head>
     <body>
-        <h1>Dainų Tekstai</h1>
-        <h2> Paragrafo Daina </h2>
-        <p>Aš toks vienišas jaunas paragrafas...</p>
-        <p><?php // print 'Mindau svetimo kodo gatves...'; ?><p>
+        <h1>
+            Valdas - Galbūt turėsiu <?php print rand(1, 8); ?> vaikų(us)!
+        </h1>
+        <p>
+            D. Trumpas nebus prezidentu: 
+                <?php 
+                    print date('Y', strtotime('+' . rand(2, 10) . 'years')) . ', ';
+                    print date('m') . ', ';
+                    print date('d'); 
+                ?>
+        </p>
     </body>
 </html>
