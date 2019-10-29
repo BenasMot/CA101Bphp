02bc38a5a3e15c1e8d6c2bdaf67a081e005344d4
valdas-v1 <valdas00722@gmail.com>
2019-10-29 17:13:43 +0200

    uzduotis #4

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 date_default_timezone_set('Europe/Vilnius');
 ?>
-
 <html lang='en'>
     <head>
         <title>PHP lydės ir <?php print date('l', strtotime('+1 day')); ?></title>
     </head>
     <body>
-        <h1>Valdas - PHP su manim buvo ir <?php print date('h', strtotime('-1 hour')) . ' valandą!'; ?></h1>
+        <h1>
+            Valdas - PHP su manim buvo ir <?php print date('h', strtotime('-1 hour')) . ' valandą!'; ?>
+        </h1>
         <p>
-            <?php print date('Y', strtotime('+1 year')) ?> ne už kalnų!
+            <?php print date('Y', strtotime('+1 year')); ?> ne už kalnų!
         </p>
     </body>
 </html>
