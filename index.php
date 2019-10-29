a1a00268d21631d3d929ee20759e6d4876adda8a
valdas-v1 <valdas00722@gmail.com>
2019-10-29 15:59:26 +0200

    uzduotis #2

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
+<?php 
+    date_default_timezone_set('Europe/Vilnius'); 
+?>
+
 <html lang="en">
 <head>
-    <title>Aš valdau HTML ir PHP!</title>
+    <title>Aš valdau HTML <?php print "ir PHP"?>!</title>
 </head>
 <body>
-    <h1>Valdas - HTML ir PHP asas!</h1>
+    <h1>Valdas - HTML <?php print "ir PHP"?> asas!</h1>
     <p>Kuriu dinaminį turinį.</p>
-        <p> Atidaryta: <?php 
+        <p> Atidaryta: 
+            <?php 
             date_default_timezone_set('Europe/Vilnius');
-            print date("Y/m/d h:i:s") ?></p>
+            print date("Y/m/d h:i:s"); 
+            ?>
+        </p>
 </body>
 </html>
