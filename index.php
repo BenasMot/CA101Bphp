97f7cd70ab6f435e7775a5d9ab72d0bfc93ea75e
valdas-v1 <valdas00722@gmail.com>
2019-10-29 16:39:37 +0200

    uzduotis #3

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<?php 
-    date_default_timezone_set('Europe/Vilnius'); 
+<?php
+date_default_timezone_set('Europe/Vilnius');
 ?>
 
-<html lang="en">
-<head>
-    <title><?php print "Aš, " . date("l") . " ir PHP"; ?></title>
-</head>
-<body>
-    <h1>Valdas - HTML <?php print "ir PHP"?> asas <br> jau nuo <?php print date("Y") . " metų"; ?></h1>
-        <p>Viskas prasidėjo 
-            <?php
-            print date("F") . " mėnesio, " . date("j") . " dieną!"; 
-            ?>
+<html lang='en'>
+    <head>
+        <title><?php print 'Aš, ' . date('l') . ' ir PHP'; ?></title>
+    </head>
+    <body>
+        <h1>Valdas - HTML <?php print 'ir PHP'; ?> asas <br> jau nuo <?php print date('Y') . ' metų'; ?></h1>
+        <p>
+            Viskas prasidėjo <?php print date('F') . ' mėnesio, ' . date('j') . ' dieną!'; ?>
         </p>
-</body>
+    </body>
 </html>
