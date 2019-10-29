94512c81b986f57ca8f265c231ea803e6694ebc0
valdas-v1 <valdas00722@gmail.com>
2019-10-29 15:01:25 +0200

    uzduotis #1

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-    print '(ne)Veikia!';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aš valdau HTML!</title>
+</head>
+<body>
+    <h1><strong>Valdas</strong> - HTML asas!</h1>
+    <p>Moku kurti statinį turinį.</p>
+</body>
+</html>
