024ecb654090b320f8c1809758a7274ede521f09
valdas-v1 <valdas00722@gmail.com>
2019-10-30 15:36:09 +0200

    Fine tuning

--- index.php
+++ index.php
@@ -3,36 +3,28 @@
         <style>
             .dice-1 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_1-512.png");
-                height: 100%;
-                width: 100%;
                 background-repeat: no-repeat;
             }
             .dice-2 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_2-512.png");
-                height: 100%;
-                width: 100%;
                 background-repeat: no-repeat;
             }
             .dice-3 {
                 background-image: url("https://cdn3.iconfinder.com/data/icons/line/36/dice_three-512.png");
-                height: 100%;
-                width: 100%;
                 background-repeat: no-repeat;
             }
             .dice-4 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png");
-                height: 100%;
-                width: 100%;
                 background-repeat: no-repeat;
             }
             .dice-5 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png");
-                height: 100%;
-                width: 100%;
                 background-repeat: no-repeat;
             }
             .dice-6 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png");
+            }
+            .img {
                 height: 100%;
                 width: 100%;
                 background-repeat: no-repeat;
@@ -40,7 +32,7 @@
         </style>
     </head>
     <body> 
-        <div class="dice-<?php print rand(1, 6); ?>">
+        <div class="img dice-<?php print rand(1, 6); ?>">
         </div>
     </body>
 </html>
