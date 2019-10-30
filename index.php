12b7e99e0f794ced8ced1331e1005eb241eee771
valdas-v1 <valdas00722@gmail.com>
2019-10-30 15:26:09 +0200

    Fine tuning

--- index.php
+++ index.php
@@ -1,19 +1,46 @@
 <html lang='en'>
     <head>
         <style>
-            
-            body {background-color: rgb(<?php print rand(0,255)?>, <?php print rand(0,255)?>, <?php print rand(0,255); ?>);}
-            h1 {font-size: <?php print rand(1,50)?>px;}
-            p {color: rgb(<?php print rand(0,255)?>, <?php print rand(0,255)?>, <?php print rand(0,255); ?>);}
-            
+            .dice-1 {
+                background-image: url("https://www.clipartwiki.com/clipimg/detail/126-1269474_dice-clipart-number-one-1-side-of-dice.png");
+                height: 100%;
+                width: 100%;
+                background-repeat: no-repeat;
+            }
+            .dice-2 {
+                background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDXs6WwDEAPbIBZupe-_GlrU64mTozgcyXPWVWxXOI5ytwBQNC&s");
+                height: 100%;
+                width: 100%;
+                background-repeat: no-repeat;
+            }
+            .dice-3 {
+                background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/240px-Dice-3-b.svg.png");
+                height: 100%;
+                width: 100%;
+                background-repeat: no-repeat;
+            }
+            .dice-4 {
+                background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png");
+                height: 100%;
+                width: 100%;
+                background-repeat: no-repeat;
+            }
+            .dice-5 {
+                background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png");
+                height: 100%;
+                width: 100%;
+                background-repeat: no-repeat;
+            }
+            .dice-6 {
+                background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png");
+                height: 100%;
+                width: 100%;
+                background-repeat: no-repeat;
+            }
         </style>
     </head>
     <body> 
-        <h1>
-            Aš keičiu savo dydį!
-        </h1>
-        <p>
-            Aš keičiu savo spalvą!
-        </p>
+        <div class="dice-<?php print rand(1, 6); ?>">
+        </div>
     </body>
 </html>
