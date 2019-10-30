bb59c8ffdb68dc181ba43f3691d99307e73b0278
valdas-v1 <valdas00722@gmail.com>
2019-10-30 16:54:08 +0200

    Fine tuning

--- index.php
+++ index.php
@@ -10,17 +10,25 @@
                 background-image: url("https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png");
             }
             .box {
-                width: 100vw;
-                height: 100vh;
+                width: 100%;
+                height: 100%;
+            }
+            .img-00 {
+                width: 100%;
+                height: 100%;
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
+                background-image: url("https://upload.wikimedia.org/wikipedia/commons/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg");
             }
         </style>
     </head>
     <body> 
         <div class="box">
-            <div class="img">
+            <div class="img img-<?php print date('s'); ?>">
             </div>
             <div>
-                <?php print date('s'); ?>
+                img-<?php print date('s'); ?>
             </div>
         </div>    
     </body>
