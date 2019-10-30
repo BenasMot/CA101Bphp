016a08616e59361e64f7abf5e71661887392b9d5
valdas-v1 <valdas00722@gmail.com>
2019-10-30 15:43:20 +0200

    Fine tuning

--- index.php
+++ index.php
@@ -3,23 +3,18 @@
         <style>
             .dice-1 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_1-512.png");
-                background-repeat: no-repeat;
             }
             .dice-2 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_2-512.png");
-                background-repeat: no-repeat;
             }
             .dice-3 {
                 background-image: url("https://cdn3.iconfinder.com/data/icons/line/36/dice_three-512.png");
-                background-repeat: no-repeat;
             }
             .dice-4 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png");
-                background-repeat: no-repeat;
             }
             .dice-5 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png");
-                background-repeat: no-repeat;
             }
             .dice-6 {
                 background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png");
